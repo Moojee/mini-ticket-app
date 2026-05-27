ac008bf65ba0cabd2962761b1f9392592609d787
moojee <sutitamoojee@gmail.com>
2026-05-28 00:27:17 +0700

    edit redirect

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -12,8 +12,8 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        // redirectTo: `${window.location.origin}/api/auth/callback`,
-        redirectTo: `http://localhost:3000/api/auth/callback`,
+        redirectTo: `${window.location.origin}/api/auth/callback`,
+        // redirectTo: `http://localhost:3000/api/auth/callback`,
       },
     })
   }
